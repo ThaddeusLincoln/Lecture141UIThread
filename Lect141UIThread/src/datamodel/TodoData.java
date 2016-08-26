af092dc90759ec5c857fc76b551ad03bcbf324fe
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-27 01:34:11 +0200

    Moved TodoItem and TodoData to separate the logic from the data.

--- Lect141UIThread/src/datamodel/TodoData.java
+++ Lect141UIThread/src/datamodel/TodoData.java
@@ -1,4 +1,4 @@
-package sample;
+package datamodel;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- Lect141UIThread/src/datamodel/TodoItem.java
+++ Lect141UIThread/src/datamodel/TodoItem.java
@@ -1,4 +1,4 @@
-package sample;
+package datamodel;
 
 import java.time.LocalDate;
 
